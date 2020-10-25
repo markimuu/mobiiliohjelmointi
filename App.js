964eba145877fcb5fe807998ec006a9783666dd3
Laiskis <kuutokm@gmail.com>
2020-10-25 21:41:33 +0200

    adding image

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+       <Image source={logo} style={{ width: 305, height: 159 }} /> 
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
