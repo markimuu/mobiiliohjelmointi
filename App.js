d64de9fcaa05f87653814370cbe67c1a15798ff9
Laiskis <kuutokm@gmail.com>
2020-11-01 02:43:24 +0200

    calculator added

--- App.js
+++ App.js
@@ -2,16 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import logo from './assets/logo.png';
+import Calculator from './root.js/Calculator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-       <Image source={logo} style={{ width: 305, height: 159 }} /> 
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Calculator></Calculator>
   );
 }
+
+/* <View style={styles.container}>
+      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+      <Text>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View> */
 
 const styles = StyleSheet.create({
   container: {
