a44447f7a23afb7890f23fe350876b7edca99158
Laiskis <kuutokm@gmail.com>
2020-11-01 02:37:45 +0200

    Revert "calculator added"
    This reverts commit 2a5c5be7a1efac4f8004fac8e141a9ec0ef4183a.

--- App.js
+++ App.js
@@ -2,19 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import logo from './assets/logo.png';
-import Calculator from './root.js/Calculator';
 
 export default function App() {
   return (
-    <Calculator></Calculator>
-  );
-}
-
-/* <View style={styles.container}>
-      <Image source={logo} style={{ width: 305, height: 159 }} /> 
+    <View style={styles.container}>
+       <Image source={logo} style={{ width: 305, height: 159 }} /> 
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
-    </View> */
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
